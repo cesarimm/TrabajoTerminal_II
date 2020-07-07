e87f8f6944bab2c6d5db4d77dd154a81b7a21b3c
Cesar Ivan Mtz <bmwcesarim@gmail.com>
2020-07-06 22:32:32 -0500

    ListView & ProgressBar

--- Rediseno3D/app/src/main/java/com/example/rediseno3d/ProgresoActivity.java
+++ Rediseno3D/app/src/main/java/com/example/rediseno3d/ProgresoActivity.java
@@ -2,9 +2,11 @@ package com.example.rediseno3d;
 
 import Procesamiento.Preprocesamiento;
 import Procesamiento.Procesamiento;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import Procesamiento.*;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -22,6 +24,8 @@ public class ProgresoActivity extends AppCompatActivity {
     TextView tv;
     ArrayList<String> lista;
     int tipo;
+    String errores = "";
+    boolean flagError=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,33 +40,41 @@ public class ProgresoActivity extends AppCompatActivity {
         if(b!=null){
             lista = (ArrayList<String>) b.get("Lista");
             tipo= (int) b.get("Tipo");
+            this.elegirTipo(tipo);
+        }else{
+            ///Se va a mandar un error - Fallo de fotografías
+            flagError = true;
+            errores+="Capturar fotografías";
         }
 
-        String aux = "";
+            ///Mandar a vaciar la lista de imagenes
+            limpiarLista();
 
-        for(int i=0;i<lista.size();i++){
-            aux+=lista.get(i)+" \n";
+            ////Mostrar errores
+        if(flagError){
+            mostrarDialogErrores();
         }
 
+    }
+
+    private void elegirTipo(int tipo){
         if(tipo==1){
+            errores+="Solidos\n";
             procesoTipo1(28);
             procesoTipo1(60);
         }else if(tipo==2){
+            errores+="Cilindros\n";
             procesoTipo2(4);
             procesoTipo2(30);
         }else if(tipo==3){
+            errores+="Huecos\n";
             procesoTipo3(5);
             procesoTipo3(15);
         }else if(tipo==4){
+            errores+="Complejos\n";
             procesoTipo4(14);
             procesoTipo4(60);
         }
-
-        ///lista.clear();
-        tv =(TextView) findViewById(R.id.textViewArchivos);
-        tv.setText(aux);
-        CameraActivity.listaImagenes.clear();
-        lista.clear();
     }
 
      ////Objetos solidos
@@ -125,138 +137,165 @@ public class ProgresoActivity extends AppCompatActivity {
             Archivos.crearArchvoOBJ(nombreArchivo, sOBJ);
 
         }catch(Exception e){
-
-
-
+            flagError = true;
+            if(divisiones==28){
+                errores+="Creación de OBJ calidad baja\n";
+            }else if(divisiones==60){
+                errores+="Creación de OBJ calidad alta\n";
+            }
         }
     }
 
     ///Cilindros
     private void procesoTipo2(int divisiones){
-        Mat mat = Preprocesamiento.preProceamiento(lista.get(0));
 
-        ArrayList<Point> aux = Procesamiento.convexHull(mat);
+        try{
+            Mat mat = Preprocesamiento.preProceamiento(lista.get(0));
 
-        aux = Herramientas.ordenarPuntos(aux);
-        double longitud = Herramientas.getLongitud(aux);
+            ArrayList<Point> aux = Procesamiento.convexHull(mat);
 
-        ArrayList<Point> auxMat = SubRectangulos.cilindrosEjeY((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
-                (int) Herramientas.min - 5, (int) Herramientas.max + 5,
-                mat, divisiones);
+            aux = Herramientas.ordenarPuntos(aux);
+            double longitud = Herramientas.getLongitud(aux);
 
-        //Toast.makeText(getApplicationContext(), ""+auxMat.size(), Toast.LENGTH_SHORT).show();
+            ArrayList<Point> auxMat = SubRectangulos.cilindrosEjeY((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
+                    (int) Herramientas.min - 5, (int) Herramientas.max + 5,
+                    mat, divisiones);
 
-        String sObj = SintaxisOBJ.generarSintaxisCilindrosOBJ(auxMat);
+            //Toast.makeText(getApplicationContext(), ""+auxMat.size(), Toast.LENGTH_SHORT).show();
+
+            String sObj = SintaxisOBJ.generarSintaxisCilindrosOBJ(auxMat);
 
 
-        String nombreArchivo="";
+            String nombreArchivo="";
 
-        if(divisiones==4){
-            nombreArchivo = generarNombreArchivo("baja");
-        }else if(divisiones==30){
-            nombreArchivo = generarNombreArchivo("alta");
+            if(divisiones==4){
+                nombreArchivo = generarNombreArchivo("baja");
+            }else if(divisiones==30){
+                nombreArchivo = generarNombreArchivo("alta");
+            }
+
+            Archivos.crearArchvoOBJ(nombreArchivo, sObj);
+
+        }catch(Exception e){
+            flagError = true;
+            if(divisiones==4){
+                errores+="Creación de OBJ calidad baja\n";
+            }else if(divisiones==30){
+                errores+="Creación de OBJ calidad alta\n";
+            }
         }
-
-        Archivos.crearArchvoOBJ(nombreArchivo, sObj);
-        Toast.makeText(getApplicationContext(), "Archivo creado", Toast.LENGTH_SHORT).show();
     }
 
 
 
     ///Engranes, tuercas, etc
     private void procesoTipo3(int mod){
-        String sOBJ="";
-        ArrayList<Mat> listaMat = new ArrayList<>();
-        ArrayList<ArrayList<Point>> listaPuntos = new ArrayList<>();
+        try{
+            String sOBJ="";
+            ArrayList<Mat> listaMat = new ArrayList<>();
+            ArrayList<ArrayList<Point>> listaPuntos = new ArrayList<>();
 
 
-        ///Proprocesar todos las imagens
-        for (int i=0 ; i <lista.size() ; i++) {
-            listaMat.add(Preprocesamiento.preProceamiento(lista.get(i)));
+            ///Proprocesar todos las imagens
+            for (int i=0 ; i <lista.size() ; i++) {
+                listaMat.add(Preprocesamiento.preProceamiento(lista.get(i)));
+            }
+
+            ///Aqui hay error en el convexHull
+            ///Agregar el convex hull para el reconocimiento de bordes
+            for (int i = 0; i <listaMat.size() ; i++) {
+                listaPuntos.add(Procesamiento.convexHull(listaMat.get(i)));
+            }
+
+            ///Ordenar las diferentes listas de puntos
+            for (int i = 0; i <listaMat.size() ; i++) {
+                listaPuntos.set(i, Herramientas.ordenarPuntos(listaPuntos.get(i)));
+            }
+
+            ////Obtener los datos mas importantes
+            Herramientas.getLongitud(listaPuntos.get(0));
+            ArrayList<Point> puntos= SubRectangulos.generarContornoXY((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
+                    (int) Herramientas.min - 5, (int) Herramientas.max + 5,
+                    listaMat.get(0));
+
+            ///Limpiar los puntos
+            puntos = Herramientas.ordenarPuntos(puntos);
+            puntos = Herramientas.limpiarListaModulo(mod, puntos);
+
+            sOBJ = SintaxisOBJ.sintaxisOBJDonas(puntos, Herramientas.getOnlyLongitud(listaPuntos.get(1)));
+
+            String nombreArchivo="";
+            if(mod==5){
+                nombreArchivo = generarNombreArchivo("alta");
+            }else if(mod==15){
+                nombreArchivo = generarNombreArchivo("baja");
+            }
+
+            Archivos.crearArchvoOBJ(nombreArchivo, sOBJ);
+        }catch(Exception e){
+            flagError = true;
+            if(mod==5){
+                errores+="Creación de OBJ calidad alta\n";
+            }else if(mod==15){
+                errores+="Creación de OBJ calidad baja\n";
+            }
         }
 
-        ///Aqui hay error en el convexHull
-        ///Agregar el convex hull para el reconocimiento de bordes
-        for (int i = 0; i <listaMat.size() ; i++) {
-            listaPuntos.add(Procesamiento.convexHull(listaMat.get(i)));
-        }
-
-        ///Ordenar las diferentes listas de puntos
-        for (int i = 0; i <listaMat.size() ; i++) {
-            listaPuntos.set(i, Herramientas.ordenarPuntos(listaPuntos.get(i)));
-        }
-
-        ////Obtener los datos mas importantes
-        Herramientas.getLongitud(listaPuntos.get(0));
-        ArrayList<Point> puntos= SubRectangulos.generarContornoXY((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
-                (int) Herramientas.min - 5, (int) Herramientas.max + 5,
-                listaMat.get(0));
-
-        ///Limpiar los puntos
-        puntos = Herramientas.ordenarPuntos(puntos);
-        puntos = Herramientas.limpiarListaModulo(mod, puntos);
-
-        sOBJ = SintaxisOBJ.sintaxisOBJDonas(puntos, Herramientas.getOnlyLongitud(listaPuntos.get(1)));
-
-        String nombreArchivo="";
-        if(mod==5){
-            nombreArchivo = generarNombreArchivo("alta");
-        }else if(mod==15){
-            nombreArchivo = generarNombreArchivo("baja");
-        }
-
-       Archivos.crearArchvoOBJ(nombreArchivo, sOBJ);
-       Toast.makeText(getApplicationContext(), "Archivo creado", Toast.LENGTH_SHORT).show();
     }
 
 
     ////Solidos complejos
     private void procesoTipo4(int divisiones){
 
-        ///Para el lado XY
-        Mat mat = Preprocesamiento.preProceamiento(lista.get(0));
+        try{
+            ///Para el lado XY
+            Mat mat = Preprocesamiento.preProceamiento(lista.get(0));
 
-        ArrayList<Point> aux = Procesamiento.convexHull(mat);
+            ArrayList<Point> aux = Procesamiento.convexHull(mat);
 
-        aux = Herramientas.ordenarPuntos(aux);
-        double longitud = Herramientas.getLongitud(aux);
-        ArrayList<Point> listaXY = SubRectangulos.generarPuntosX((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
-                (int) Herramientas.min - 5, (int) Herramientas.max + 5,
-                longitud, divisiones, mat);
+            aux = Herramientas.ordenarPuntos(aux);
+            double longitud = Herramientas.getLongitud(aux);
+            ArrayList<Point> listaXY = SubRectangulos.generarPuntosX((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
+                    (int) Herramientas.min - 5, (int) Herramientas.max + 5,
+                    longitud, divisiones, mat);
 
-        //Para el lado YZ
-        Mat mat2 = Preprocesamiento.preProceamiento(lista.get(1));
+            //Para el lado YZ
+            Mat mat2 = Preprocesamiento.preProceamiento(lista.get(1));
 
-        ArrayList<Point> aux2 = Procesamiento.convexHull(mat2);
+            ArrayList<Point> aux2 = Procesamiento.convexHull(mat2);
 
-        aux = Herramientas.ordenarPuntos(aux2);
-         longitud = Herramientas.getLongitud(aux2);
-        ArrayList<Point> listaYZ = SubRectangulos.generarPuntosX((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
-                (int) Herramientas.min - 5, (int) Herramientas.max + 5,
-                longitud, divisiones, mat2);
+            aux = Herramientas.ordenarPuntos(aux2);
+            longitud = Herramientas.getLongitud(aux2);
+            ArrayList<Point> listaYZ = SubRectangulos.generarPuntosX((int) Herramientas.yMin - 5, (int) Herramientas.yMax + 5,
+                    (int) Herramientas.min - 5, (int) Herramientas.max + 5,
+                    longitud, divisiones, mat2);
 
 
-        mat2 = Preprocesamiento.preProceamiento(lista.get(2));
-        aux2 = Procesamiento.convexHull(mat2);
+            mat2 = Preprocesamiento.preProceamiento(lista.get(2));
+            aux2 = Procesamiento.convexHull(mat2);
 
-        ///Validar la distancia
+            ///Validar la distancia
             ///TODO CORRECTO
-            try{
-                String sObj = SintaxisOBJ.objXYZ(listaXY, listaYZ);
+            String sObj = SintaxisOBJ.objXYZ(listaXY, listaYZ);
 
-                String nombreArchivo="";
+            String nombreArchivo="";
 
-                if(divisiones==14){
-                    nombreArchivo = generarNombreArchivo("baja");
-                }else if(divisiones==60){
-                    nombreArchivo = generarNombreArchivo("alta");
-                }
-
-                Archivos.crearArchvoOBJ(nombreArchivo, sObj);
-            }catch(Exception e){
-                    e.printStackTrace();
+            if(divisiones==14){
+                nombreArchivo = generarNombreArchivo("baja");
+            }else if(divisiones==60){
+                nombreArchivo = generarNombreArchivo("alta");
             }
 
+            Archivos.crearArchvoOBJ(nombreArchivo, sObj);
+
+        }catch(Exception e){
+            flagError = true;
+            if(divisiones==14){
+                errores+="Creación de OBJ calidad baja\n";
+            }else if(divisiones==60){
+                errores+="Creación de OBJ calidad alta\n";
+            }
+        }
     }
 
 
@@ -265,6 +304,35 @@ public class ProgresoActivity extends AppCompatActivity {
         aux = aux.replace(".jpg", calidad+".obj");
         //aux+=calidad+".obj";
         return aux;
+     }
+
+     private void limpiarLista(){
+        ///Eliminar los archivos de la carpeta
+
+         ///Eliminar de los arrayList
+         ///lista.clear();
+         tv =(TextView) findViewById(R.id.textViewProceso);
+         tv.setText("hola");
+         CameraActivity.listaImagenes.clear();
+         lista.clear();
+     }
+
+     private void mostrarDialogErrores(){
+
+         AlertDialog.Builder build = new AlertDialog.Builder(this);
+         build.setMessage(this.errores);
+         build.setTitle("ERRORES");
+         build.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(DialogInterface dialogInterface, int i) {
+                 finish();
+                 Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                 startActivity(intent);
+             }
+         });
+
+         AlertDialog dialog = build.create();
+         dialog.show();
      }
 
 }
